7a9ea76e70532620f68e7bc9105ce161babca3e2
Davi Cintra <davi.cintra2002@gmail.com>
2021-10-01 19:32:53 -0300

    Adicionando menu principal com a lista de categorias (sem responsivo)

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 document.getElementById("signup").onclick = function () {
-  window.open("pages/cadastro.html", "_self");
+  window.open("../pages/cadastro.html", "_self");
+};
+
+document.getElementById("login").onclick = function () {
+  window.open("../pages/menuPrincipal.html", "_self");
 };
